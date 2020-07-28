c3353852d9113a69fbc2bbbeb3ec87686cc41eef
Brianna Keune <briannakeune@gmail.com>
2020-07-28 13:02:10 -0500

    solutions for two sum

---
+++ two_sum.py
@@ -0,0 +1,78 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        # nested loops, will probably time out.
+        # for j in range(0, len(nums) - 1):
+        #     for k in range(0, len(nums) - 1):
+        #         num1, num2 = nums[j], nums[k + 1]
+        #         if j is k + 1:
+        #             continue
+        #         elif num1 + num2 == target:
+        #             return [j, k + 1]
+
+        # idea 2:
+        # input: list of nums, target num
+        # output, list, of indexes that sum up to target
+        # we loop through list,
+        # create a cache of numbers, and what number will get to target
+        # when target is found, return those indexes
+
+        # ~~~~~~~~~~~ WORKING ~~~~~~~~~~~~~~~~~
+        # cache = {}
+        # location = []
+
+        # # creating cache
+        # for index, num in enumerate(nums):
+        #     if index not in cache:
+        #         cache[(index, num)] = target - num
+
+        # # finding possible solutions in cache
+        # for key, num in cache.items():
+        #     index, value = key
+        #     temp = None
+        #     if num in nums:
+        #         location.append(index)
+
+        # print('location: ', location)
+        # if len(location) == 2:
+        #     index1, index2 = location[0], location[1]
+        #     if nums[index1] + nums[index2] == target:
+        #         return location
+
+        # # searching for two indexes that sum equal to target
+        # for j in range(0, len(location) - 1):
+        #     for k in range(0, len(location) - 1):
+        #         index1, index2 = location[j], location[k + 1]
+        #         num1, num2 = nums[index1], nums[index2]
+        #         if j is k + 1:
+        #             continue
+        #         elif num1 + num2 == target:
+        #             return [index1, index2]
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        cache = {}
+        for index, num in enumerate(nums):
+            cache[num] = index
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in cache and cache[complement] != i:
+                return [cache[complement], i]
+
+
+solution = Solution()
+print(solution.twoSum([-1, -2, -3, -4, -5], -8))
